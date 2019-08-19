a349f9d62a0e84026fe84224509eaa8f7554899e
Massimiliano Masserell <mmasserelli@gmail.com>
2019-08-19 15:08:41 +0200

    Modifiche template eventi

--- wp-event-manager/content-event_listing.php
+++ wp-event-manager/content-event_listing.php
@@ -1,78 +1,74 @@
-<div  class="line-layout">
-<li  <?php event_listing_class(); ?> data-longitude="<?php echo esc_attr( $post->geolocation_lat ); ?>" data-latitude="<?php echo esc_attr( $post->geolocation_long ); ?>">
-  <div class="event-info-row-listing">
-	<a href="<?php display_event_permalink(); ?>">
-	   <div class="row">
-            <div class="col-md-1">
-                <div class="organizer-logo">
-                    <?php  display_event_banner(); ?>
-                </div>
+<?php
+$start_date = get_event_start_date();
+$end_date = get_event_end_date();
+$event_type = get_event_type();
+if (is_array($event_type) && isset($event_type[0])) {
+    $event_type = $event_type[0]->slug;
+}
+    $banner= get_event_banner();
+
+    if (is_array($banner) && isset($banner[0])) {
+        $banner = $banner[0];
+    } elseif (is_array($banner) && empty($banner)) {
+        $banner = '';
+    } else {
+        $banner = $banner;
+    }
+?>
+<div class="wpem-event-box-col wpem-col wpem-col-12 wpem-col-md-6 wpem-col-lg-4 "><!----- wpem-col-lg-4 value can be change by admin settings ------->
+  <div class="wpem-event-layout-wrapper">
+  <div <?php event_listing_class('wpem-event-layout-wrapper'); ?>>
+    <a href="<?php display_event_permalink(); ?>" class="wpem-event-action-url event-style-color <?php echo $event_type;?>">
+      <div class="wpem-event-banner">
+        <div class="wpem-event-banner-img" style="background-image: url('<?php echo $banner  ?>')">
+
+          <!-- Hide in list View // Show in Box View -->
+          <?php do_action('event_already_registered_title');?>
+          <div class="wpem-event-date">
+            <div class="wpem-event-date-type">
+              <div class="wpem-from-date">
+                <div class="wpem-date"><?php echo date_i18n('d', strtotime($start_date)); ?></div>
+                <div class="wpem-month"><?php echo date_i18n('M', strtotime($start_date)); ?></div>
+              </div>
+
             </div>
-            <div class="col-md-4">
-                <div class="event-title">
-                    <h4><?php echo esc_html( get_the_title() ); ?></h4>
-                    <div class="boxes-view-listing-registered-code">
-                        <?php do_action('event_already_registered_title');?>
-                    </div>
-                </div>
-                <div class="event-organizer-name">
-                    <?php display_organizer_name( '<normal>', '<normal>' ); ?>
-                </div>
-            </div>
-            <div class="col-md-2">
-    			   <div class="date">
-    			        <date><?php display_event_start_date();?></date>
-    			   </div>
-	        </div>
-	        <div class="col-md-3">
-		        <div class="event-location"><i class="glyphicon glyphicon-map-marker"></i>
-		          <?php if(get_event_location()=='Anywhere' || get_event_location() == ''): echo __('Online Event','wp-event-manager'); else:  display_event_location(false); endif; ?>
-		       </div>
-	        </div>
-	        <div class="col-md-2">
-                <div class="event-ticket"><?php echo '#'.get_event_ticket_option(); ?></div>
-            </div>
-            <div class="col-md-3"> <?php if ( get_option( 'event_manager_enable_event_types' ) ) { display_event_type(); } ?></div>
+          </div>
+          <!-- Hide in list View // Show in Box View -->
         </div>
-      </a>
-     </div>
-   </li>
-</div>
+      </div>
 
-<!-- Box Layout -->
-<aside class="col-sm-4">
-<a href="<?php display_event_permalink(); ?>">
- <div class="card card-default">
-   <div class="card-header"><?php the_title(); ?></div>
-   <div class="card-body card-5-7" style="height: 250px;">
-    <div class="card-left">
-      <?php  display_event_banner(); ?>
-    </div>
-    <div class="card-right">
-      da <?php display_event_start_date();?><br />
-      a <?php display_event_end_date(); ?>
-    </div>
-    <div class="card-body">
-    <?php if (!empty(get_event_description())) : ?>
-      <?php display_event_description(); ?>
-    <?php endif; ?>
-    </div>
-   </div>
-   <div class="card-footer">
-     <?php if (!empty(get_event_location())) : ?>
-       <i class="glyphicon glyphicon-map-marker"></i>
-       <?php display_event_location(false); ?>
-     <?php endif; ?>
-   </div>
+      <div class="wpem-event-infomation">
+          <div class="wpem-event-date">
+            <div class="wpem-event-date-type">
+
+              <div class="wpem-from-date">
+                <div class="wpem-date"><?php echo date_i18n('d', strtotime($start_date)); ?></div>
+                <div class="wpem-month"><?php echo date_i18n('M', strtotime($start_date)); ?></div>
+              </div>
+              <div class="wpem-to-date">
+                <div class="wpem-date-separator">-</div>
+                <div class="wpem-date"><?php echo date_i18n('d', strtotime($end_date)); ?></div>
+                <div class="wpem-month"><?php echo date_i18n('M', strtotime($end_date)); ?></div>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="wpem-event-details">
+            <div class="wpem-event-title"><h3 class="wpem-heading-text"><?php echo esc_html(get_the_title()); ?></h3></div>
+
+            <div class="wpem-event-date-time"><span class="wpem-event-date-time-text"><?php display_event_start_date();?> - <?php display_event_end_date();?></span></div>
+            <div class="wpem-event-location"><span class="wpem-event-location-text"><?php if (get_event_location()=='Anywhere' || get_event_location() == ''): echo __('Online Event', 'wp-event-manager'); else:  display_event_location(false); endif; ?></span></div>
+
+            <?php if (get_option('event_manager_enable_event_types') && get_event_type()) {
+    ?>
+              <div class="wpem-event-type"><?php display_event_type(); ?></div>
+            <?php
+} ?>
+            <?php do_action('event_already_registered_title');?>
+          </div>
+      </div>
+    </a>
   </div>
-
- </div>
-</a>
-</aside>
-<!-- Box Layout end-->
-
-<script>
-jQuery(document).ready(function($)
-{
-   ContentEventListing.init();
-});</script>
+  </div>
+</div>
