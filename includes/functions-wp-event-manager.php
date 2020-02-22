6e7f217efa02b1e72a0f36bb1af296e8b93eee78
Massimiliano Masserell <mmasserelli@gmail.com>
2020-02-22 12:02:05 +0100

    Visualizzazione in ordine degli eventi

--- includes/functions-wp-event-manager.php
+++ includes/functions-wp-event-manager.php
@@ -1,19 +1,13 @@
 <?php
 function wpem_limit_upcoming($query_args, $args) {
-  $query_args['meta_query'][] = array(
+  $query_args['meta_query']['mes_wpem'] = array(
     'key' => '_event_start_date',
     'value' => date('Y-m-d', time()),
     'compare' => '>=',
     'type' => 'date'
   );
+  $query_args[ 'orderby' ] =  array ('mes_wpem' => 'ASC'); //orderby will be according to data stored inside the particular meta key
+
   return ($query_args);
 }
 add_filter('get_event_listings_query_args', 'wpem_limit_upcoming', 20, 2);
-
-function mes_custom_orderby( $query_args ) {
-    $query_args[ 'orderby' ] = 'meta_value'; //orderby will be according to data stored inside the particular meta key
-    $query_args[ 'order' ] = 'DESC';
-    return $query_args;
-}
-
-add_filter( 'event_manager_get_listings_args', 'mes_custom_orderby', 99 );
